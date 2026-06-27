1011b8a26bf793941544a92be3da5512f698d629
Muhammad Qasim Akram <m.qasimakram46@gmail.com>
2026-06-27 06:59:14 +0500

    basic syntax

--- dotNet/Program.cs
+++ dotNet/Program.cs
@@ -5,7 +5,15 @@ namespace dotNet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please enter yuor name");
+            String name =  Console.ReadLine();
+            char symbol = '@';
+            Console.WriteLine("please enter your  age:"); 
+             int age = Convert.ToInt32(Console.ReadLine());
+            double heigh = 5.11;
+            const String moto = "Finish what you start! ";
+            Console.WriteLine("Hello! My name is " + name + " and I am " + age + " old.\nMy height is " + heigh +
+                                ".\nMy Username is " + symbol + name + " and My moto is \n\t" + moto);
         }
     }
-}
+} 
