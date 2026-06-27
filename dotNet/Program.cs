09095b4f66069234212631ff02f2716b64eb7ec2
Muhammad Qasim Akram <m.qasimakram46@gmail.com>
2026-06-27 16:08:47 +0500

    more  practice

--- dotNet/Program.cs
+++ dotNet/Program.cs
@@ -5,15 +5,26 @@ namespace dotNet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter yuor name");
+            /*Console.WriteLine("Please enter yuor name");
             String name =  Console.ReadLine();
+
             char symbol = '@';
             Console.WriteLine("please enter your  age:"); 
-             int age = Convert.ToInt32(Console.ReadLine());
+            int age = Convert.ToInt32(Console.ReadLine());
+
             double heigh = 5.11;
             const String moto = "Finish what you start! ";
             Console.WriteLine("Hello! My name is " + name + " and I am " + age + " old.\nMy height is " + heigh +
                                 ".\nMy Username is " + symbol + name + " and My moto is \n\t" + moto);
+
+            Console.ReadKey();*/
+            Console.WriteLine("enter the side a :");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the side b:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            double c = Math.Sqrt((a*a)+(b*b));
+            Console.WriteLine("Your result is : "+c);
         }
-    }
+    } 
 } 
