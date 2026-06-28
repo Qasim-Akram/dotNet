2add01dea45547b145cdbe58ec80fac879be70d8
Muhammad Qasim Akram <m.qasimakram46@gmail.com>
2026-06-28 13:37:30 +0500

    more hands on practice

--- dotNet/Program.cs
+++ dotNet/Program.cs
@@ -17,14 +17,30 @@ namespace dotNet
             Console.WriteLine("Hello! My name is " + name + " and I am " + age + " old.\nMy height is " + heigh +
                                 ".\nMy Username is " + symbol + name + " and My moto is \n\t" + moto);
 
-            Console.ReadKey();*/
+            Console.ReadKey();
             Console.WriteLine("enter the side a :");
             int a = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("enter the side b:");
             int b = Convert.ToInt32(Console.ReadLine());
 
             double c = Math.Sqrt((a*a)+(b*b));
-            Console.WriteLine("Your result is : "+c);
+            Console.WriteLine("Your result is : "+c);*/
+
+            Console.WriteLine("Enter the number of row: ");
+            int row = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number of colums: ");
+            int column = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("Enter the symbol");
+            char symbol = Convert.ToChar(Console.ReadLine());
+
+            for (int i = 0;i <= row;i++)
+            {
+                for(int j = 0;j < column; j++)
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
         }
     } 
 } 
